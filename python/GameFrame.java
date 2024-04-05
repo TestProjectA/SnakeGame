2a55d2365d11c880e20cca0d6ffe795bc1c0f361
YADAV AKASHAY BALASO <117169447+TestProjectA@users.noreply.github.com>
2024-04-05 22:21:26 +0530

    Add files via upload

---
+++ python/GameFrame.java
@@ -0,0 +1,17 @@
+package python;
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame {
+    GameFrame() {
+
+        this.add(new GamePanal());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
---
+++ python/SnakeGame.java
@@ -0,0 +1,7 @@
+package python;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+}
